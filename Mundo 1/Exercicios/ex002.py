f4f78e707eff26037c94567c7f855162b5e257da
w-farezin <willyanfn@gmail.com>
2026-01-03 15:36:22 -0300

    Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas.

---
+++ Mundo 1/Exercicios/ex002.py
@@ -0,0 +1,4 @@
+"Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas."
+
+name = input('Qual é o seu nome?')
+print('Seja bem vindo {} !, é um prazer lhe conhecer!'.format(name))
